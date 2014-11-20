4bfc199cd35dcf78fca456b1b2608d3efd8d6d64
Guitsou <guillaume@reynaud.biz>
2014-11-20 16:29:39 +0100

    Travail de la journée

--- musiciens/musiciens.php
+++ musiciens/musiciens.php
@@ -4,59 +4,30 @@
 		<meta charset="utf-8" />
 		<title>Déclaration des musiciens</title>
 		<link rel="stylesheet" type="text/css" href="styles.css">
+
+		<script language="JavaScript">
+		function toggleContent(){
+			// Get the DOM reference
+			var contentId = document.getElementById('sub3');
+			// Toggle
+			contentId.style.display == "block" ? contentId.style.display = "none" : contentId.style.display = "block";
+		}
+		</script>
+
 	</head>
 	
 	<body>
 
 	<?php
 
-	if (($_POST['musicien']) AND ($_POST['instrument'])){
-		echo "<p>POST musicien : " . $_POST['musicien'] . "</p>";
-		echo "<p>POST instrument : " . $_POST['instrument'] . "</p>";
-		echo "<br /><hr />";
-		//INSERT INTO `musique`.`personnes_instruments` (`id_personnes`, `id_instruments`) VALUES ('2', '2');
-	}	
-
 	// Connexion à la base
-	$bdd = new PDO('mysql:host=localhost;dbname=musique', 'root', '') or die(mysql_error());
+	include("connexion.php");
 
 	// Requete pour populer la liste
 	$liste_musiciens = $bdd->query('SELECT id, prenom, nom FROM personnes ORDER BY prenom, nom') or die(mysql_error());
 	$liste_instruments = $bdd->query('SELECT id, nom FROM instruments ORDER BY nom') or die(mysql_error());
-
-	// Formulaire
-	?>
-	<form action="musiciens.php" method="post" class="smart-green">
-		<h1>Formulaire Musiciens
-			<span>Merci de remplir le formulaire d'ajout d'instruments.</span>
-		</h1>
-
-	<?php
-	// Liste déroulante musiciens
-	echo "<label><span>Musiciens : </span><select name='musicien'>";
-	while ( $musicien = $liste_musiciens->fetch()) 
-	{
-		echo '<option value=\'' . $musicien['id'] . '\'>' . htmlspecialchars($musicien['prenom']) . ' ' . htmlspecialchars($musicien['nom']) . '</option>';
-	}
-	echo "</select></label>";
-	$liste_musiciens->closeCursor();
-
-	// Liste déroulante instruments
-	echo "<label><span>Instruments : </span><select name='instrument'>";
-	while ( $instrument = $liste_instruments->fetch())
-	{
-		echo '<option value=\'' . $instrument['id'] . '\'>' . htmlspecialchars(utf8_encode($instrument['nom'])) . '</option>';
-	}
-	echo "</select></label>";
-	$liste_instruments->closeCursor();	
-
 	?>
 
-		<p>
-			<label><span>&nbsp;</span>
-			<input type="submit" class="button" value="Créer" />
-		</p>
-	</form>
 
 	<?php
 
@@ -68,30 +39,68 @@
 		WHERE p_i.id_instruments = i.id AND p_i.id_personnes = p.id') or die(mysql_error());
 	?>
 
-	<table>
+	<br />
+	
+	<div class="tableau">
+		<table>
+			<caption>Liste des musiciens</caption>
 
-		<caption>Liste des musiciens et instruments</caption>
-
-		<thead> <!-- Entête tableau -->
+			<!-- Entête -->
 			<tr>
-				<th>Musicien</th>
-				<th>Instrument</th>
-				<th>Actions</th>
+			<td>Musicien</td>
+			<td>Instrument</td>
+			<td>Actions</td>
 			</tr>
 
-		<tbody>
-			<tr>
 			<?php
 				while ( $resultat = $liste_musiciens_instruments->fetch() ){
-					echo "<td>" . $resultat['prenom'] . "</td>\n" . 
-						"<td>" . $resultat['instru'] . "</td>\n" . 
-						"<td>" . "test" . "</td>\n";
+					echo "<tr>\n" .
+						"<td>" . utf8_encode($resultat['prenom']) . "</td>\n" . 
+						"<td>" . utf8_encode($resultat['instru']) . "</td>\n" . 
+						"<td>" . "test" . "</td>\n" .
+						"</tr>\n";
 				}
 				$liste_musiciens_instruments->closeCursor();
 			?>
-			</tr>
-		</tbody>
 	</table>
+	<input type="image" src="add.png" alt="Nouveau" align="center" name="new_entry" onclick="toggleContent()"/>
+</div>
+
+<!-- Formulaire -->
+<br />
+<div id="sub3" style="display:none;">
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" class="smart-green">
+	<h1>Formulaire Musiciens
+		<span>Merci de remplir le formulaire d'ajout d'instruments.</span>
+	</h1>
+
+<?php
+// Liste déroulante musiciens
+echo "<label><span>Musiciens : </span><select name='musicien'>";
+while ( $musicien = $liste_musiciens->fetch()) 
+{
+	echo '<option value=\'' . $musicien['id'] . '\'>' . htmlspecialchars($musicien['prenom']) . ' ' . htmlspecialchars($musicien['nom']) . '</option>';
+}
+echo "</select></label>";
+$liste_musiciens->closeCursor();
+
+// Liste déroulante instruments
+echo "<label><span>Instruments : </span><select name='instrument'>";
+while ( $instrument = $liste_instruments->fetch())
+{
+	echo '<option value=\'' . $instrument['id'] . '\'>' . htmlspecialchars(utf8_encode($instrument['nom'])) . '</option>';
+}
+echo "</select></label>";
+$liste_instruments->closeCursor();	
+
+?>
+
+	<p>
+		<input type="submit" class="button" value="Créer" />
+	</p>
+</form>
+</div>
+
 
 	</body>
 
