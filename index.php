08a00962100ed041344674b1c0d6179ca36110d0
Guitsou <guillaume@reynaud.biz>
2014-12-02 16:04:16 +0100

    modif css

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once('modele/connexion_sql.php');
+
+if (!isset($_GET['section']) OR $_GET['section'] == 'index')
+{
+    include_once('controleur/referentiel/index.php');
+}
+
+if ($_GET['section'] == 'detail')
+{
+	include_once('controleur/referentiel/detail.php');
+}
